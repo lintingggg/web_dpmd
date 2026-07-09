afd3207d3fe8461d7078ab847624786533a4b45c
Ariq-Ikbar-Hakim <ariq20055@gmail.com>
2026-07-09 14:48:59 +0700

    design: Menambahkan .env.example

--- resources/js/Components/MobileMenu.vue
+++ resources/js/Components/MobileMenu.vue
@@ -104,7 +104,7 @@
 import { ref, watch, onBeforeUnmount } from 'vue';
 import { Button, TextField, Avatar } from '@idds/vue';
 import { IconSearch, IconBell, IconChevronDown, IconMenu2, IconX } from '@tabler/icons-vue';
-import type { NavItem } from './MainNavigation.vue';
+import type { NavItem } from './Navbar.vue';
 
 interface Props {
   items: NavItem[];
