02b5089a72676cea9d80656864ef30592efd58ec
lintingggg <imuh6500@gmail.com>
2026-07-22 16:55:43 +0700

    feat: implement responsive Navbar component with scroll behavior and mobile drawer

--- resources/js/Components/Navbar/Navbar.vue
+++ resources/js/Components/Navbar/Navbar.vue
@@ -34,7 +34,7 @@ onBeforeUnmount(() => {
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor">
                         <path fill-rule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd" />
                     </svg>
-                    Jl. HALIM PERDANA KUSUMA NO. 4 MLAJAH BANGKALAN
+                    Jl. Halim Perdana Kusuma No. 4 Mlajah Bangkalan 69111 Bangkalan
                 </span>
             </div>
             <div class="flex items-center gap-4">
